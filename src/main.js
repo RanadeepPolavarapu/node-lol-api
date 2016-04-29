014a7f07c74aa341442cc9db2b075fe1e0065dcd
Ranadeep Polavarapu <ranadeep@icloud.com>
2016-04-29 17:43:26 -0400

    babel preset es2015, add until leagues routes

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import querystring from 'querystring';
 import requestPromise from 'request-promise';
 import request from 'request';
 import url from 'url';
+import util from 'util';
 
 class LoLAPIClient {
     constructor(apiKey, region = 'NA') {
@@ -62,24 +63,13 @@ class LoLAPIClient {
     }
 
     _makeAPIRequest(url) {
-        console.log(url);
-        let rpOptions = {
+        let requestPromiseOptions = {
             uri: url,
             json: true,
         };
-        let response;
 
-        requestPromise(rpOptions)
-            .then(body => {
-                response = body;
-            })
-            .catch(err => {
-                response = err;
-            });
-
-        console.log(response);
-
-        return response;
+        let apiRequestPromise = requestPromise(requestPromiseOptions);
+        return apiRequestPromise;
     }
 
     _buildAPIRequestURL(route, region = this.region, options = {}) {
@@ -91,10 +81,16 @@ class LoLAPIClient {
         let routeQuerystring = querystring.stringify(options);
         let finalURLFormatted = url.resolve(baseURLFormatted, routeFormatted) +
             '?' + routeQuerystring;
+
+        LOGGER.debug(`fn: ${this._buildAPIRequestURL.name}(
+            route: ${route},
+            region: ${region},
+            options: ${util.inspect(options)}): URL=${finalURLFormatted}`);
+
         return finalURLFormatted;
     }
 
-    getChampions(region, freeToPlay) {
+    getAllChampions(region, freeToPlay) {
         let route = `/api/lol/{region}/v1.2/champion`;
 
         let finalURL = this._buildAPIRequestURL(route, region, {
@@ -104,14 +100,134 @@ class LoLAPIClient {
         return this._makeAPIRequest(finalURL);
     }
 
-    getChampionById(region, championId = 1) {
+    getChampionByChampionId(region, championId = 1) {
         let route = `/api/lol/{region}/v1.2/champion/${championId ? championId : 1}`;
         let finalURL = this._buildAPIRequestURL(route, region);
 
-        console.log(finalURL);
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getChampionMasteryBySummonerIdAndChampionId(region, summonerId, championId) {
+        let platformId = this.REGIONAL_ENDPOINTS[region.toUpperCase()].platformId;
+        let route =
+            `/championmastery/location/${platformId}/player/${summonerId}/champion/${championId}`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getAllChampionMasteryEntriesBySummonerId(region, summonerId) {
+        let platformId = this.REGIONAL_ENDPOINTS[region.toUpperCase()].platformId;
+        let route =
+            `/championmastery/location/${platformId}/player/${summonerId}/champions`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getTotalChampionMasteryScoreBySummonerId(region, summonerId) {
+        let platformId = this.REGIONAL_ENDPOINTS[region.toUpperCase()].platformId;
+        let route =
+            `/championmastery/location/${platformId}/player/${summonerId}/score`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getTopChampionMasteryEntriesBySummonerId(region, summonerId, count) {
+        let platformId = this.REGIONAL_ENDPOINTS[region.toUpperCase()].platformId;
+        let route =
+            `/championmastery/location/${platformId}/player/${summonerId}/topchampions`;
+        let finalURL = this._buildAPIRequestURL(route, region, {
+            count: count,
+        });
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getCurrentGameBySummonerId(region, summonerId) {
+        let platformId = this.REGIONAL_ENDPOINTS[region.toUpperCase()].platformId;
+        let route =
+            `/observer-mode/rest/consumer/getSpectatorGameInfo/${platformId}/${summonerId}`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getFeaturedGames(region) {
+        let route = `/observer-mode/rest/featured`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getRecentGamesBySummonerId(region, summonerId) {
+        let route = `/api/lol/${region}/v1.3/game/by-summoner/${summonerId}/recent`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getLeaguesBySummonerIds(region, summonerIds=[]) {
+        let route = `/api/lol/${region}/v2.5/league/by-summoner/${summonerIds}`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getLeagueEntryBySummonerIds(region, summonerIds=[]) {
+        let route = `/api/lol/${region}/v2.5/league/by-summoner/${summonerIds}/entry`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getLeaguesByTeamIds(region, teamIds=[]) {
+        let route = `/api/lol/${region}/v2.5/league/by-team/${teamIds}`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getLeagueEntryByTeamIds(region, teamIds=[]) {
+        let route = `/api/lol/${region}/v2.5/league/by-team/${teamIds}/entry`;
+        let finalURL = this._buildAPIRequestURL(route, region);
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getLeaguesChallengerTier(region, queueType='RANKED_SOLO_5x5') {
+        let route = `/api/lol/${region}/v2.5/league/challenger`;
+        let finalURL = this._buildAPIRequestURL(route, region, {
+            type: queueType,
+        });
+
+        return this._makeAPIRequest(finalURL);
+    }
+
+    getLeaguesMasterTier(region, queueType='RANKED_SOLO_5x5') {
+        let route = `/api/lol/${region}/v2.5/league/master`;
+        let finalURL = this._buildAPIRequestURL(route, region, {
+            type: queueType,
+        });
+
+        return this._makeAPIRequest(finalURL);
     }
 }
 
 let lol = new LoLAPIClient('abee5b6a-41b5-4be4-8d50-bd19cd4da6d5', 'NA');
-LOGGER.info(lol.getChampions('TR'));
-LOGGER.info(lol.getChampionById('kr'));
+
+// lol.getChampions('TR', true).then(response => console.log(response));
+lol.getChampionByChampionId('TR', 2)
+    .then(response => console.log(response))
+    .catch(err => LOGGER.error(err));
+
+lol.getChampionMasteryBySummonerIdAndChampionId('na', 5908, 1)
+    .then(response => console.log(response))
+    .catch(err => LOGGER.error(err));
+
+lol.getLeaguesBySummonerIds('na', [5908, 2, 8])
+    .then(response => console.log(response))
+    .catch(err => LOGGER.error(err));
+
+// LOGGER.info(lol.getChampionById('kr'));
